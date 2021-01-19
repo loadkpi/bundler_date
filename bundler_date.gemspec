ebd54c0fac6557d25c9993aa443e122424a70a7a
loadkpi <loadkpi@gmail.com>
2021-01-19 21:07:07 +0300

    plugin base

--- bundler_date.gemspec
+++ bundler_date.gemspec
@@ -26,7 +26,9 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "rake",    "~> 13.0"
-  spec.add_dependency "rspec",   "~> 3.0"
-  spec.add_dependency "rubocop", "~> 1.7"
+  spec.add_dependency "bundler", "~> 2.0"
+
+  spec.add_development_dependency "rake",    "~> 13.0"
+  spec.add_development_dependency "rspec",   "~> 3.0"
+  spec.add_development_dependency "rubocop", "~> 1.7"
 end
--- lib/bundler_date.rb
+++ lib/bundler_date.rb
@@ -3,6 +3,14 @@
 require_relative "bundler_date/version"
 
 module BundlerDate
-  class Error < StandardError; end
-  # Your code goes here...
+  class Command
+    # The exec method will be called with the `command_name` and the `args`.
+    # This is where you should handle all logic and functionality
+    def exec(command_name, args)
+      puts "You called " + command_name + " with args: " + args.inspect
+    end
+  end
+
+  # Register this class as a handler for the `date` command
+  Bundler::Plugin::API.command("date", Command)
 end
